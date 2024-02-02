1567091f76a1ef3e593d221f5193557cc9ad025e
danaxsax <dana.salinas.tareas@gmail.com>
2024-02-02 07:52:23 -0600

    feat cleaning data function

--- exploring.py
+++ exploring.py
@@ -1,28 +1,20 @@
 import pandas as pd
 
 df = pd.read_csv("spotify-2023.csv", encoding='latin1')
-#print(df.head(8))
+#print(df.info())
+#print(df.dtypes)
 #print(df['artist(s)_name'].value_counts)
 #print(df.notna().all())
 #print(df[df['in_shazam_charts'].isnull()])
 #print(df["artist(s)_name"]ss)
-print(f"original dataframe")
-print(df.head(8))
-print(f"****************************************************************")
+#print(f"original dataframe")
+#print(df.head(8))
+#print(f"****************************************************************")
 
-print(f"columns with null values")
-print((df.notna().all()))
-print(f"****************************************************************")
+#print(f"columns with null values")
+#print((df.notna().all()))
+#print(f"****************************************************************")
+#print(df["bpm"].max())
 
-print(f"dataframe with marked null values for empty registers")
-df_filled = df.fillna(value=0)
-print (df_filled)
-print(f"****************************************************************")
-
-print(f"dataframe full")
-nan_rows_after = df_filled[df_filled.isna().any(axis=1)]
-print(nan_rows_after)
-print(f"****************************************************************")
-
-
-
+#bpm_160 = df[df["bpm"]>160]
+#print (bpm_160)
---
+++ manipulation.py
@@ -0,0 +1,19 @@
+import pandas as pd
+df = pd.read_csv("spotify-2023.csv", encoding='latin-1')
+#1.- check data unique rows, unique series
+def cleaning():
+    if df.index.is_unique & df.columns.is_unique:
+        print("unique rows and columns!")
+    else:
+        dup = df.duplicated()
+        for index, value in dup.items():
+            if value:
+                print(f"The duplicated series are: {df.loc[index]}")
+        df.drop_duplicates(inplace=True)
+
+cleaning()
+
+        
+
+        
+    
