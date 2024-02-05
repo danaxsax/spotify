472aa284864d33b50c4c8ef472825da1043820e9
danaxsax <dana.salinas.tareas@gmail.com>
2024-02-05 17:14:33 -0600

    feat: menu and functions

--- manipulation.py
+++ manipulation.py
@@ -1,25 +1,78 @@
 import pandas as pd
 import plotly.express as px
+pd.options.plotting.backend = "plotly"
 
-#cargar el csv
-df = pd.read_csv("spotify-2023.csv", encoding='latin-1')
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-NaN_values = df.isna() #True donde hay valores NaN
-any_missing_values = NaN_values.any().any() #verifica si hay NaN en cualquier parte del df
-missing_value_columns = NaN_values.any() #retorna True para columnas con NaN row
-missing_value_counts = df.isnull().sum() #suma de la cantidad de registros NaN por columna
+def taylors_version(df):
+    tays= df[df["artist(s)_name"] == "Taylor Swift"]
+    print(tays)
+    fig_dates= px.parallel_coordinates(tays, dimensions=["released_year", "released_month", "released_day"],color='in_spotify_charts',  title = "historial de taylor swift's most impresive records released date")
+    fig_dates.show()
+    fig_info = px.parallel_coordinates(tays, dimensions=["danceability_%", "valence_%", "energy_%", "acousticness_%", "instrumentalness_%", "liveness_%","speechiness_%"], title= "caracteristicas de las canciones mas top de tay", color="in_spotify_charts")
+    fig_info.show()
 
-#outputs
-print(f"Hay valores faltantes? {any_missing_values}\n")
-print(f"Valores faltantes \n{missing_value_columns}\n")
-for column, has_missing in missing_value_columns.items():
-    if has_missing:
-        count = missing_value_columns[column]
-        print(f"------> Columna '{column}' : {count}")
-print(f"\nRegistros faltantes por columna \n{missing_value_counts}")
+def to_the_hits(df):
+    hit = df[""]
+    print()
 
-#cleaning
-df.dropna(inplace=True) #eliminas registros con NaN
-df.info()
+def streams_playlist(df):
+    print()
 
+def top_playlist(df):
+    print()
+def dates (df):
+    print()
+def duos(df):
+    print()
+def top_per_year(df):
+    print()
+
+def cleaning_and_menu():
+    import pandas as pd
+    import plotly.express as px
+    pd.options.plotting.backend = "plotly"
+    #cargar el csv
+    df = pd.read_csv("spotify-2023.csv", encoding='latin-1')
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    NaN_values = df.isna() #True donde hay valores NaN
+    any_missing_values = NaN_values.any().any() #verifica si hay NaN en cualquier parte del df
+    missing_value_columns = NaN_values.any() #retorna True para columnas con NaN row
+    missing_value_counts = df.isnull().sum() #suma de la cantidad de registros NaN por columna
+    print(f"Hay valores faltantes? {any_missing_values}\n")
+    print(f"Valores faltantes \n{missing_value_columns}\n")
+    for column, has_missing in missing_value_columns.items():
+        if has_missing:
+            count = missing_value_columns[column]
+            print(f"------> Columna '{column}' : {count}")
+    print(f"\nRegistros faltantes por columna \n{missing_value_counts}")
+    print("\n⋆ ˚｡⋆୨୧˚✩₊˚.⋆☾⋆⁺₊✧Magia de limpieza⋆ ˚｡⋆୨୧˚✩₊˚.⋆☾⋆⁺₊✧\n")
+    #cleaning
+    df.dropna(inplace=True) #eliminas registros con NaN
+    df.info()
+
+    print("＊*•̩̩͙✩•̩̩͙*˚*•̩̩͙✩•̩̩͙*˚＊⭒⭒*•̩̩͙✩•̩̩͙*˚⍣˚*•̩̩͙✩•̩̩͙*˚＊⭒Hola•̩̩͙✩•̩̩͙*˚⍣˚*•̩̩͙✩•̩̩͙*˚˚⍣＊⭒˚⍣⭒＊*•̩̩͙✩•̩̩͙*˚⍣˚")
+    print("1. Taylor Swift most succesfull records !")
+    print("2. Caracteristicas que debería tener tu record para triunfar")
+    print("3. Se relacionan los streams con las veces que se guardó en playlist?")
+    print("4. Top songs basada en las veces que se guardó en playlist")
+    print("5. Fechas en que más se saca musica")
+    print("6. Los duos más chambeadores (artistas que colaboraron más)")
+    print("7. La canción top por año")
+    selected = int(input("Qué opcion quieres? "))
+
+    dictionary = {
+        1: taylors_version(df),
+        2: to_the_hits(df),
+        3: streams_playlist(df),
+        4: top_playlist(df),
+        5: dates(df),
+        6: duos(df),
+        7: top_per_year(df)
+    }
+    if selected in dictionary:
+        return dictionary[selected](df)
+        
+
+
+
+cleaning_and_menu()
